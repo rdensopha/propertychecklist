167c04ded4072aa44253c70731f23545e7c5dd3b
rdensopha <rdensopha@yahoo.com>
2013-03-28 16:43:40 +0530

    property checklist first push

---
+++ app/models/category.rb
@@ -0,0 +1,8 @@
+#this domain model has attributes #name, #status
+#any object of Category  has @category.subcategories and @category.parentCategory
+#the table categories has column -- parentCategory_id
+class Category < ActiveRecord::Base
+  attr_accessible :name, :status
+  has_many :subcategories, class_name: 'Category', foreign_key: 'parentCategory_id'
+  belongs_to :parentCategory, class_name: 'Category'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Propertychecklist::Application.config.secret_token = '52e2b687424eea8de84330e046611cc8a32b971edb71f4a86901c03a9cacb6817674caa72e10372e8f2b8e5a6fda8948f8752223a9a02bf9c7cf78346b2472fd'
