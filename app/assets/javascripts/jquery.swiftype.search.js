cea2381b852c68357e41c963459045ec1aa35e90
rdensopha <rdensopha@yahoo.com>
2013-04-18 17:11:48 +0530

    check search bar working on heroku

--- app/assets/javascripts/jquery.swiftype.search.js
+++ app/assets/javascripts/jquery.swiftype.search.js
@@ -4,25 +4,24 @@
     '$:nomunge'; // Used by YUI compressor.
 
     // Reused string.
-    var str_hashchange = 'hashchange',
+    var str_hashchange = 'hashchange';
 
     // Method / object references.
-        doc = document,
-        fake_onhashchange,
-        special = $.event.special,
+       var doc = document;
+        var fake_onhashchange, special = $.event.special;
 
     // Does the browser support window.onhashchange? Note that IE8 running in
     // IE7 compatibility mode reports true for 'onhashchange' in window, even
     // though the event isn't supported, so also test document.documentMode.
-        doc_mode = doc.documentMode,
-        supports_onhashchange = 'on' + str_hashchange in window && ( doc_mode === undefined || doc_mode > 7 );
+        var doc_mode = doc.documentMode;
+        var supports_onhashchange = 'on' + str_hashchange in window && ( doc_mode === undefined || doc_mode > 7 );
 
     // Get location.hash (or what you'd expect location.hash to be) sans any
     // leading #. Thanks for making this necessary, Firefox!
     function get_fragment( url ) {
         url = url || location.href;
         return '#' + url.replace( /^[^#]*#?(.*)$/, '$1' );
-    };
+    }
 
     // Method: jQuery.fn.hashchange
     //
@@ -207,98 +206,9 @@
             }
 
             timeout_id = setTimeout( poll, $.fn[ str_hashchange ].delay );
-        };
+        }
 
-        // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        // vvvvvvvvvvvvvvvvvvv REMOVE IF NOT SUPPORTING IE6/7/8 vvvvvvvvvvvvvvvvvvv
-        // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        /*$.browser.msie && !supports_onhashchange && (function(){
-            // Not only do IE6/7 need the "magical" Iframe treatment, but so does IE8
-            // when running in "IE7 compatibility" mode.
 
-            var iframe,
-                iframe_src;
-
-            // When the event is bound and polling starts in IE 6/7, create a hidden
-            // Iframe for history handling.
-            self.start = function(){
-                if ( !iframe ) {
-                    iframe_src = $.fn[ str_hashchange ].src;
-                    iframe_src = iframe_src && iframe_src + get_fragment();
-
-                    // Create hidden Iframe. Attempt to make Iframe as hidden as possible
-                    // by using techniques from http://www.paciellogroup.com/blog/?p=604.
-                    iframe = $('<iframe tabindex="-1" title="empty"/>').hide()
-
-                        // When Iframe has completely loaded, initialize the history and
-                        // start polling.
-                        .one( 'load', function(){
-                            iframe_src || history_set( get_fragment() );
-                            poll();
-                        })
-
-                        // Load Iframe src if specified, otherwise nothing.
-                        .attr( 'src', iframe_src || 'javascript:0' )
-
-                        // Append Iframe after the end of the body to prevent unnecessary
-                        // initial page scrolling (yes, this works).
-                        .insertAfter( 'body' )[0].contentWindow;
-
-                    // Whenever `document.title` changes, update the Iframe's title to
-                    // prettify the back/next history menu entries. Since IE sometimes
-                    // errors with "Unspecified error" the very first time this is set
-                    // (yes, very useful) wrap this with a try/catch block.
-                    doc.onpropertychange = function(){
-                        try {
-                            if ( event.propertyName === 'title' ) {
-                                iframe.document.title = doc.title;
-                            }
-                        } catch(e) {}
-                    };
-
-                }
-            };
-
-            // Override the "stop" method since an IE6/7 Iframe was created. Even
-            // if there are no longer any bound event handlers, the polling loop
-            // is still necessary for back/next to work at all!
-            self.stop = fn_retval;
-
-            // Get history by looking at the hidden Iframe's location.hash.
-            history_get = function() {
-                return get_fragment( iframe.location.href );
-            };
-
-            // Set a new history item by opening and then closing the Iframe
-            // document, *then* setting its location.hash. If document.domain has
-            // been set, update that as well.
-            history_set = function( hash, history_hash ) {
-                var iframe_doc = iframe.document,
-                    domain = $.fn[ str_hashchange ].domain;
-
-                if ( hash !== history_hash ) {
-                    // Update Iframe with any initial `document.title` that might be set.
-                    iframe_doc.title = doc.title;
-
-                    // Opening the Iframe's document after it has been closed is what
-                    // actually adds a history entry.
-                    iframe_doc.open();
-
-                    // Set document.domain for the Iframe document as well, if necessary.
-                    domain && iframe_doc.write( '<script>document.domain="' + domain + '"</script>' );
-
-                    iframe_doc.close();
-
-                    // Update the Iframe's hash, for great justice.
-                    iframe.location.hash = hash;
-                }
-            };
-
-        })();
-        // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        // ^^^^^^^^^^^^^^^^^^^ REMOVE IF NOT SUPPORTING IE6/7/8 ^^^^^^^^^^^^^^^^^^^
-        // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-*/
         return self;
     })();
 
