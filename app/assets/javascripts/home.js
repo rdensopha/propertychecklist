cd908603f17b84b1c8590aa1abcbfd0cceedd5f3
rdensopha <rdensopha@yahoo.com>
2013-04-24 15:52:45 +0530

    cleaning up asset pipeline

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into home.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -10,7 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery.icheck
-
-
-
+//= require jquery.ha-hashchange
+//= require swiftype
