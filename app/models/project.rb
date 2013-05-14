5e93bf7b5795a4e5bb3680d715d225c936953c57
rdensopha <rdensopha@yahoo.com>
2013-05-14 10:23:04 +0530

    basic project specs

--- app/models/project.rb
+++ app/models/project.rb
@@ -16,10 +16,19 @@
 
 class Project < ActiveRecord::Base
  extend FriendlyId
-  attr_accessible :name, :project_developer_id, :city_id
+  attr_accessible :name, :project_developer_id, :city_id, :status
   belongs_to :project_developer
   has_many :project_checklist_responses
   belongs_to :city
   #setting the friendly id option
   friendly_id :name, use: :slugged
+  
+  #validations
+  validates :name, presence: true
+  validates(:status,
+            presence: true,
+            inclusion:{
+              in: [APP_CONFIG.fetch('active'), APP_CONFIG.fetch('inactive')],
+              message:"%{value} is not a valid status"
+            })
 end
---
+++ spec/models/project_spec.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id                   :integer          not null, primary key
+#  name                 :string(255)
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  status               :string(255)
+#  project_developer_id :integer
+#  projectType          :string(255)
+#  projectLocation      :string(255)
+#  city_id              :integer
+#  slug                 :string(255)
+#
+
+require 'spec_helper'
+
+describe 'Project' do
+  it "is valid with valid name and status" do
+    testProj = Project.new(name: 'TestProject', status: APP_CONFIG['active'])
+    expect(testProj).to be_valid
+  end
+  
+  it "is invalid without a name" do
+    testProj = Project.new(name: nil, status: APP_CONFIG['active'])
+    expect(testProj).to have(1).errors_on(:name)
+  end
+  
+  it "is invalid with a invalid status value" do
+     testProj = Project.new(name: 'TestProject', status: 'crazyStatus')
+     expect(testProj).to have(1).errors_on(:status)
+  end
+end
